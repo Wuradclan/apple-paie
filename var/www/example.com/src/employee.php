4701f38dba8191d50fb7bf0bd8e47bfd8771b8b1
WuRAD <morad.aitabdellah@gmail.com>
2023-12-09 23:42:48 +0100

    localhost deleted and replaced by root url /

--- var/www/example.com/src/employee.php
+++ var/www/example.com/src/employee.php
@@ -3,6 +3,6 @@ $connectbdd = mysqli_connect("mysql", "example_db_user", "example_db_password", 
 $sql = $connectbdd->query("SELECT * FROM employee");
 while($donnees = $sql->fetch_assoc())
 {
-echo $donnees[header('Location: http://localhost:8080/fiche-de-paie-master/')];
+echo $donnees[header('Location: /fiche-de-paie-master/')];
 }
 ?>
